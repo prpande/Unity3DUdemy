6b072f05e1be798396313654f2274a577787dfee
Pratyush Pande <pratyush.pande@gmail.com>
2022-08-06 03:05:46 -0700

    save

--- LiftOff/Assets/Scripts/Movement.cs
+++ LiftOff/Assets/Scripts/Movement.cs
@@ -1,18 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private new Rigidbody rigidbody;
+    [SerializeField] private float thrustMultiplier = 1500.0f;
+    [SerializeField] private float rotationMultiplier = 100.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(thrustMultiplier * Time.deltaTime * Vector3.up);
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        var rotationAmount = rotationMultiplier * Time.deltaTime * Vector3.forward;
+        rigidbody.freezeRotation = true;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(rotationAmount);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-rotationAmount);
+        }
+        rigidbody.freezeRotation = false;
     }
 }
